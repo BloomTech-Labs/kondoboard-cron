5a13440d00ff5b57b8b5f2011a99edad1442b049
Mark <mark.a.mcadam@gmail.com>
2020-06-10 13:25:51 -0700

    copies etl .py files into src app

--- src/app/main.py
+++ src/app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+
+
 app = FastAPI()
+
+from .extract import merge_all_apis
+from .transform import transform_df
+from .load import query
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,3 +27,14 @@ async def root():
     <h1>Kondoboard Cron Deployed</h1>
     <p>Go to <a href="/docs">/docs</a> for documentation.</p>
     """)
+
+
+@app.get("/start")
+def start_upload(): #async 
+    """
+    Start the cron task to upload new jobs to the elasticsearch database
+    """
+    df = merge_all_apis()
+    df = transform_df(df)
+    df = query(df)
+    return 'Cron job complete'
---
+++ src/app/transform.py
@@ -0,0 +1,63 @@
+import arrow
+from bs4 import BeautifulSoup
+
+
+def keyword(text):
+    """
+    Formats keywords by 
+    -lowercasing all letters 
+    -replacing ' ' with '_'
+    -replacing '-' with '_'
+    """
+    text = str(text).lower()
+    text = text.replace("-", " ")
+    return text
+
+
+def remove_html(text):
+    """
+    Removes HTML
+    """
+    if type(text) == str:
+        return BeautifulSoup(text, "html.parser").get_text()
+    else:
+        return [BeautifulSoup(x, "html.parser").get_text() for x in text]
+
+
+def format_date(text):
+    """
+    Formats Date to 'YYYY-MM-DD'
+    """
+    time = arrow.get(text)
+    time = time.format("YYYY-MM-DD")
+    return time
+
+
+cols_to_remove_html = ["title", "title_keyword", "description"]
+date_cols = ["publication_date"]
+keyword_cols = ["company", "title_keyword"]
+
+
+def transform_df(df):
+    """
+    Takes in dataframe, applies functions to correct columns
+    """
+    # insert date column
+    df["inserted_date"] = arrow.utcnow().format("YYYY-MM-DD")
+
+    # remove html from columns
+    for col in cols_to_remove_html:
+        df[col] = df[col].apply(remove_html)
+
+    # transforms date columns
+    for col in date_cols:
+        df[col] = df[col].apply(format_date)
+
+    # transform keyword columns
+    for col in keyword_cols:
+        df[col] = df[col].apply(keyword)
+
+    for col in ["latitude", "longitude"]:
+        df[col] = df[col].fillna(0)
+
+    return df
